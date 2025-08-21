e14bc45c0ea6b38132e009fc9ee028cf6abf7bb8
Anirudha Donge <anirudha.donge@gmail.com>
2025-08-21 08:31:06 +0530

    Assertions and Waits

---
+++ PlaywrightTs/Assertions/Assertions.ts
@@ -0,0 +1,15 @@
+/**
+ * Assertion: Validation of Expected State.
+ * expect
+ * -- General Assertions
+ *    : we are performing the validation on the end values (scalar property)
+ *      examples :
+ *           
+ * -- Locator Assertions: these assertions are perform on the locator.
+ *    : you create locator object then you check the state of that locator.
+ * -- Page Assertions
+ *    : Validation performed on the page
+ *      await expect(page).toHaveTitle('')
+ *      await expect(page).toHaveUrl('')
+ * -- soft Asserts
+ */
--- PlaywrightTs/tests/firstTest.spec.ts
+++ PlaywrightTs/tests/firstTest.spec.ts
@@ -273,7 +273,7 @@ test("Drag and Drop", async ({ page }) => {
   await expect(page.locator("#column-a")).toHaveText('B');
 });
 
-test.only("Hovers Test", async ({ page }) => {
+test("Hovers Test", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com");
   await page.locator('[href="/hovers"]').click();
   await page.getByAltText('User Avatar').nth(0).hover()
@@ -281,3 +281,34 @@ test.only("Hovers Test", async ({ page }) => {
   const text = await page.locator(".example p").textContent();
   console.log(text);
 });
+
+test("Hovers Test with waitsforTimeout", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com");
+  const locator = page.locator('[href="/hovers"]');
+  //await locator.waitFor({state:'attached',timeout:3000});
+  await page.waitForLoadState()
+  await locator.click();
+  await page.getByAltText('User Avatar').nth(0).hover()
+  await expect(page.locator(".figcaption h5").nth(0)).toHaveText("name: user1");
+  const text = await page.locator(".example p").textContent();
+  expect(text).not.toBe("Hover over the image for additional information");
+});
+
+
+test.only("Hover with Locator assertions", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com");
+  const locator = page.locator('[href="/hovers"]');
+  //await locator.waitFor({state:'attached',timeout:3000});
+  await page.waitForLoadState()
+  await locator.click();
+  await page.getByAltText('User Avatar').nth(0).hover()
+  const captionLocator = page.locator(".figcaption h5").nth(0);
+  await expect.soft(captionLocator).toBeVisible();
+  await expect.soft(captionLocator).toHaveText("name: user1"); // Locator Assertions
+  const text = await page.locator(".example p").textContent();
+  expect.soft(text).not.toBe("Hover over the image for additional information"); // General Assertion
+  await expect.soft(page).toHaveTitle('The Internet');
+  await expect.soft(page).toHaveURL('https://the-internet.herokuapp.com/hovers');
+  console.log(page.url());
+});
+
